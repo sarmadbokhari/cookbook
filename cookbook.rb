b774b62bc3184d6a98fb636e21ce639445cdced4
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-03-04 20:01:28 -0600

    add cookbook and recipe getter and setter methods the long way

--- cookbook.rb
+++ cookbook.rb
@@ -2,6 +2,14 @@ class Cookbook
   def initialize(title)
     @title = title
   end
+
+  def title=(new_title)
+    @title = new_title
+  end
+
+  def title
+    @title
+  end
 end
 
 class Recipe
@@ -10,4 +18,29 @@ class Recipe
     @ingredients = ingredients
     @steps = steps
   end
+
+  def title
+    @title
+  end
+
+  def ingredients
+    @ingredients
+  end
+
+  def steps
+    @steps
+  end
+
+  def title=(new_title)
+    @title = new_title
+  end
+
+  def ingredients=(new_ingredients)
+    @ingredients = new_ingredients
+  end
+
+  def steps=(new_steps)
+    @steps = new_steps
+  end
+
 end
