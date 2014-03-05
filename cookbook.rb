0cd2339ca58764e111d5c4a896b18673958a3288
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2014-03-04 20:06:47 -0600

    add attr shorter getter setter methods

--- cookbook.rb
+++ cookbook.rb
@@ -1,46 +1,22 @@
 class Cookbook
+  attr_accessor :title
+
   def initialize(title)
     @title = title
   end
 
-  def title=(new_title)
-    @title = new_title
-  end
-
-  def title
-    @title
-  end
 end
 
 class Recipe
+  attr_reader :title
+  attr_writer :steps
+  attr_accessor :ingredients
+
   def initialize(title, ingredients, steps)
     @title = title
     @ingredients = ingredients
     @steps = steps
   end
 
-  def title
-    @title
-  end
-
-  def ingredients
-    @ingredients
-  end
-
-  def steps
-    @steps
-  end
-
-  def title=(new_title)
-    @title = new_title
-  end
-
-  def ingredients=(new_ingredients)
-    @ingredients = new_ingredients
-  end
-
-  def steps=(new_steps)
-    @steps = new_steps
-  end
 
 end
